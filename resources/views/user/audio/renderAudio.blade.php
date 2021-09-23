c000fc3e87391102114024e568e96dec9f1263aa
muzahir5 <muzahirhussain74@yahoo.com>
2021-09-23 17:04:15 +0500

    Next & Prev in audio player

--- resources/views/user/audio/renderAudio.blade.php
+++ resources/views/user/audio/renderAudio.blade.php
@@ -1,8 +1,8 @@
 @extends('user.layouts.default')
 @section('title', 'Audios')
 @section('content')
-    <h2>{{$category->name}}</h2>
-    <div class="col-lg-6">	    
+    <h2>{{$category->name}} <span name="cate_id" id="cate_id" style="display: none;">{{$cat_id}}</span></h2>
+    <!-- <div class="col-lg-6">	    
 	    <div class="form-group">
 	        <label>Type a name</label>
 	        <input type="text" name="audio" id="audioo" placeholder="Enter audio name" class="form-control">
@@ -11,32 +11,52 @@
 	    <div id="country_list">
 	    	<ul> <li></li> </ul>
 	    </div>                    
-	</div> 
-	    @if($audios)
-	    	<div class="list-group def_audios">
-                <?php $i = 0?>
-	    	@foreach($audios as $audio)
-			    <span class="list-group-item list_audio"style="margin: 1px;">
-			      <h4 class="list-group-item-heading">{{$audio->title}}</h4>
-			      <p class="list-group-item-text">{{$category->name}}
-			      	<i class="fas fa-play list_play" onclick="playaudio(<?php echo $i;?>)" style="float: right;"></i> </p>
-			    </span>   		
-    			<?php $i++?>
-	    	@endforeach
-	    	</div>
-	    @endif
+	</div>  -->
+	    	<table id="data_tbl" class="display" style="width:100%">
+                <thead>
+                    <tr>
+                    <th scope="col"></th>
+                    </tr>
+                    </thead>
+                <tbody>
+                  @if($audios)
+                  <?php $i = 0?>
+                    @foreach($audios as $audio)
+                      <tr id="{{$i}}">
+                        <td>
+                        <div class="list-group def_audios">
+                            <span class="list-group-item list_audio"style="margin: 1px;">
+                                <h4 class="list-group-item-heading">{{$audio->title}}  </h4>
+                                <p class="list-group-item-text">{{$category->name}}
+                                <i class="fas fa-play list_play" onclick="playaudio(<?php echo $i;?>)" style="float: right;"></i> </p>
+                            </span> 
+                        </div>
+                        </td>
+                      </tr>
+                      <?php $i++?>
+                      @endforeach
+                  @endif 
+                </tbody>
+            </table>
 @endsection
 
 @include('dashboard.includes.foot')
 
+<script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js" defer></script>
+
 <script type="text/javascript">
     $(document).ready(function () {
 
+        $('#data_tbl').DataTable( {
+                "pagingType": "full_numbers",
+                "pageLength": 50
+            } );                    
+
     	//load js song arr by cat_id
-    	var cate_id = $('#cate_id').val();
+    	var cate_id = $('#cate_id').text();
 
     	if(cate_id > 0){
-    		// console.log(cate_id);
+    		console.log(cate_id);
     		$.ajax({               
                 url:"{{ route('user.listAudioByCatagoryId') }}" + '/'+ + cate_id,
                 type:"GET",               
@@ -45,16 +65,19 @@
                     // console.log(data);
                     JSON.stringify(data); //to string
                     var html = [];
+                    var current = 0;
                     $.each(data.audios, function(audio) {
                    	 // console.log(data.audios[audio]);
+                        songs.push(current);
 				        songsTitle.push(data.audios[audio].title);
 				        songsImage.push(data.audios[audio].audio_img);
 				        songsSrc.push(data.audios[audio].audio_url);
+
+                        current++;        
 				    });
                 }
             });
     	}
-
     	
 
     	$('#country_list').css('display','none');     
@@ -78,9 +101,7 @@
                         html +='<li><span title="'+ data.audios[audio].title +'" target="_blank">'+ data.audios[audio].title + ' <i class="fas fa-play list_play" onclick="playaudio('+data.audios[audio].id+')" style="float: right;"></i> </span></li>';
                         songsTitle.push(data.audios[audio].title);
                         songsImage.push(data.audios[audio].audio_img);
-                        songsSrc.push(data.audios[audio].audio_url);
-
-                        
+                        songsSrc.push(data.audios[audio].audio_url);                        
 				    });
 
                     
@@ -120,7 +141,14 @@
     function playaudio(audio_index)
     {
         // console.log(audio_index);    console.log(songsImage[audio_index]);   console.log(songsSrc[audio_index]);
+        songIndex = audio_index;
         loadSong(audio_index);
         playSong();
+
+        $("#data_tbl tbody").on("click", "tr", function(){
+        // console.log(audio_index +' id ');        
+        
+
+    });
     }
 </script>

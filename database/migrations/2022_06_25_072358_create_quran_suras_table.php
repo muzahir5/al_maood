b3235108834b1f6974dbd5ceef9383fb0e7bb0e1
Muzahir Hussain <muzahir.hussain@therightsw.com>
2022-06-25 12:58:56 +0500

    createing table of Quaran Surah

---
+++ database/migrations/2022_06_25_072358_create_quran_suras_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuranSurasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quran_suras', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('surah_number');
+            $table->integer('aya_number');
+            $table->integer('status')->default(1);
+            $table->text('arabic_text');
+            $table->text('translation');
+            $table->text('footnotes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quran_suras');
+    }
+}
